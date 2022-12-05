8cfa9005ea520751131a360d4b661c3b0e6097e1
Snehal979 <bansodsnehal535@gmail.com>
2022-12-05 22:30:30 +0530

    (snehal)Add:Given message to analyse and response

--- MoodAnalyserPro/MoodAnalyserPro/Program.cs
+++ MoodAnalyserPro/MoodAnalyserPro/Program.cs
@@ -5,8 +5,8 @@
         public static void Main(string[] args)
         {
             Analyser analyser = new Analyser();
-            string msg = analyser.CheckMoodAnalyser("I am in Sad Mood");
-            Console.WriteLine(msg);
+            string msgs = analyser.CheckMoodAnalyser("I am in Sad Mood");
+            Console.WriteLine(msgs);
         }
     }
 }
