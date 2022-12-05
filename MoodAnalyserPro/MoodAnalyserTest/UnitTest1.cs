ce27bf4d8c8a65561973b8482f3d1f516fae9e6e
Snehal979 <bansodsnehal535@gmail.com>
2022-12-05 22:42:18 +0530

    (Snehal)Add: test case for given any message and return happy

--- MoodAnalyserPro/MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserPro/MoodAnalyserTest/UnitTest1.cs
@@ -8,15 +8,21 @@ namespace MoodAnalyserTest
         [Test]
         public void GivenSadMood_whenAnalyser_ReturnTheSadMessage()
         {
-           
-            
             //act
             string result = analyser.CheckMoodAnalyser("I am in a Sad mood");
 
             Assert.AreEqual("Sad", result);
         }
-      
-       
+        [Test]
+        public void GivenAnyMood_whenAnalyser_ReturnTheHappyMessage()
+        {
+            //act
+            string result = analyser.CheckMoodAnalyser("I am in a Any mood");
+
+            Assert.AreEqual("Happy", result);
+        }
+
+
 
     }
 }
