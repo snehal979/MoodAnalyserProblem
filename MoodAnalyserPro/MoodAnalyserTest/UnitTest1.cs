c1de9ad9e5f55fe738d8be2bb5f3b3cd0282c783
Snehal979 <bansodsnehal535@gmail.com>
2022-12-05 22:38:16 +0530

    (Snehal)Add: test case for given sad message and return sad

--- MoodAnalyserPro/MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserPro/MoodAnalyserTest/UnitTest1.cs
@@ -1,13 +1,22 @@
+using MoodAnalyserPro;
+
 namespace MoodAnalyserTest
 {
     public class Tests
     {
+        Analyser analyser = new Analyser();
+        [Test]
+        public void GivenSadMood_whenAnalyser_ReturnTheSadMessage()
+        {
+           
+            
+            //act
+            string result = analyser.CheckMoodAnalyser("I am in a Sad mood");
+
+            Assert.AreEqual("Sad", result);
+        }
+      
        
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
     }
 }
